a9f1748a0d594c80e7f41402ec14e54e1cb8ce83
fighteros <fighteros121212@gmail.com>
2021-09-26 04:33:06 +0200

    state init and class component  based app

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,10 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+/* 
 const App = () => {
+
+    // get current physical position
+    window.navigator.geolocation.getCurrentPosition(
+        // at success of getting location
+        (position) => console.log(position),
+        // at failure of getting location
+        (err) => console.log(err)
+    );
+
   return <div>
-      Hi There!
+      Latitude: 
   </div>  
-};
+}; */
+
+
+class App extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        // to initialize state
+        this.state = {
+            lat : null, 
+        };
+    }
+
+    // React requirement 
+    render() {
+        // get current physical position
+         window.navigator.geolocation.getCurrentPosition(
+        // at success of getting location
+        (position) => console.log(position),
+        // at failure of getting location
+        (err) => console.log(err)
+    );
+        return <div>Latitude: </div>  
+    }  
+}
 
 ReactDOM.render(<App /> , document.querySelector('#root'))
