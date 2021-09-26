ff15d81f592c386208b0d9d49b28416154a8f277
fighteros <fighteros121212@gmail.com>
2021-09-26 13:52:01 +0200

    before showing icons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9){
+        return lat > 0 ? 'summer': 'winter';
+    }
+    else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
 
 const SeasonDisplay = ({ lat }) => {
-    return <div>
-        Latitude: {lat }
+    const season = getSeason(lat, new Date().getMonth())
+    const text = season === 'winter'? 'Burr, it is chilly' : 'Let\'s hit the beach'
+    return (
+    <div>
+        <h1>{text}</h1>
     </div>
+    );
 };
 
 export default SeasonDisplay;
