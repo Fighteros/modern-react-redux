c5b6a72fc61704633d4c56323e1e34ea327033df
fighteros <fighteros121212@gmail.com>
2021-09-28 05:57:57 +0200

    Extracting Options to Config Objects

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
+const seasonConfig = {
+    summer: {
+        text: 'Let\'s hit the beach',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr, it is chilly',
+        iconName: 'snowflake'
+    }
+}
+
 const getSeason = (lat, month) => {
-    if(month > 2 && month < 9){
-        return lat > 0 ? 'summer': 'winter';
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
     }
     else {
         return lat > 0 ? 'winter' : 'summer';
@@ -12,11 +23,16 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = ({ lat }) => {
     const season = getSeason(lat, new Date().getMonth())
-    const text = season === 'winter'? 'Burr, it is chilly' : 'Let\'s hit the beach'
+    // const text = season === 'winter' ? 'Burr, it is chilly' : 'Let\'s hit the beach'
+    // const icon = season === 'winter' ? 'snowflake' : 'sun';
+    const { text, iconName } = seasonConfig[season];
+
     return (
-    <div>
-        <h1>{text}</h1>
-    </div>
+        <div>
+            <i className={`icon ${iconName}`} />
+            <h1>{text}</h1>
+            <i className={`icon ${iconName}`} />
+        </div>
     );
 };
 
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -19,28 +19,28 @@ const App = () => {
 
 
 class App extends React.Component {
-/*     
-    constructor(props) {
-        super(props);
-        // to initialize state and only time we do direct assignment
-        // to the state
-        this.state = {
-            lat : null,
-            errorMessage: ''
-        };
-    } */
+    /*     
+        constructor(props) {
+            super(props);
+            // to initialize state and only time we do direct assignment
+            // to the state
+            this.state = {
+                lat : null,
+                errorMessage: ''
+            };
+        } */
 
-    state = { lat: null, errorMessage : ''};
-    
+    state = { lat: null, errorMessage: '' };
+
     componentDidMount() {
         // get current physical position
         window.navigator.geolocation.getCurrentPosition(
             // at success of getting location
-            position => this.setState({lat: position.coords.latitude}),
+            position => this.setState({ lat: position.coords.latitude }),
             // at failure of getting location
             // when calling setState we just write things that we want to update
             //  anyother thing in the object will not be touched!
-            err => this.setState({errorMessage : err.message})
+            err => this.setState({ errorMessage: err.message })
         );
     }
     /*
@@ -52,16 +52,16 @@ class App extends React.Component {
     // React requirement 
     // render get called frequantly - performance -; 
     render() {
-        if (this.state.errorMessage && !this.state.lat){
+        if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage} </div>
         }
 
-        if(!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay  lat={this.state.lat}  />
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div>Loading !</div>
-    }  
+        return <div><span>  Loading ! <i className="notched circle loading icon"></i></span></div>
+    }
 }
 
-ReactDOM.render(<App /> , document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'))
