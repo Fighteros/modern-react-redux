b0bb7008f48f53b21b01c4e26168730917e7b632
Ahmed Abd ElGhany <am5419@fayoum.edu.eg>
2021-10-17 23:49:44 +0200

    added videos

--- videos/src/App.js
+++ videos/src/App.js
@@ -5,18 +5,25 @@ import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
 
-    onTermSubmit = (term) => {
-        youtube.get('/search', {
+    state = {
+        videos: []
+    }
+
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         });
+
+        this.setState({ videos: response.data.items })
     };
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                I have {this.state.videos.length}  videos
             </div>
         );
     }
